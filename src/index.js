9119e7f36e066aa92a6036ab65707a632de650f8
jay radadiya <jayradadiya09@gmail.com>
2023-07-26 01:54:42 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("src/index.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace(
+    "{%tempval%}",
+    (orgVal.main.temp - 273.15).toFixed(2)
+  );
+  temperature = temperature.replace(
+    "{%tempmin%}",
+    (orgVal.main.temp_min - 273.15).toFixed(2)
+  );
+  temperature = temperature.replace(
+    "{%tempmax%}",
+    (orgVal.main.temp_max - 273.15).toFixed(2)
+  );
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      `https://api.openweathermap.org/data/2.5/weather?q=Surat&appid=${process.env.APP_ID}`
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        console.log(objdata);
+        const arrData = [objdata];
+        // console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+        // console.log(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  } else {
+    res.end("File not found");
+  }
+});
+
+server.listen(8000, "127.0.0.1");
